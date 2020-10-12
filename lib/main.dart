208a79dca472e3f7659c9eb0773056aa69cbdec9
Jithin varghese <jithinvpynadath@gmail.com>
2020-10-12 22:31:25 +1300

    Transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import './transaction.dart';
+import './widgets/user_transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,10 +14,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transation = [
-    Transaction(id: 't1', title: 'shoe', amount: 89.99, date: DateTime.now()),
-    Transaction(id: 't2', title: 'phone', amount: 819.99, date: DateTime.now()),
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +21,7 @@ class MyHomePage extends StatelessWidget {
         title: Text('Expense App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        // mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -38,52 +33,7 @@ class MyHomePage extends StatelessWidget {
               shadowColor: Colors.pink,
             ),
           ),
-          Column(
-            children: transation.map((tx) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                        color: Colors.orange[200],
-                        width: 2,
-                      )),
-                      padding: EdgeInsets.all(5),
-                      child: Text(
-                        '\$ ${tx.amount.toString()}', //STRING INTEWRPOLOTION :compling the string the doller "$" is a recved caracter to give out put we can use "/""
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 12,
-                            color: Colors.orange),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tx.title,
-                          style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 16,
-                              color: Colors.black87),
-                        ),
-                        Text(
-                          DateFormat.yMMMd().format(tx.date),
-                          style: TextStyle(
-                              fontWeight: FontWeight.w300,
-                              fontSize: 12,
-                              color: Colors.black87),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
+          UserTransaction(),
         ],
       ),
     );
